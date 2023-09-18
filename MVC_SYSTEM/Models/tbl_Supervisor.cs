d02063e5eb063334649a1c7c5524c8ec3caceb27
shazanasalehudin@gmail.com <shazanasalehudin@gmail.com>
2023-09-19 00:20:45 +0800

    New screen supervisor pup estate
    New screen supervisor pup estate

---
+++ MVC_SYSTEM/Models/tbl_Supervisor.cs
@@ -0,0 +1,30 @@
+﻿using MVC_SYSTEM.App_LocalResources;
+
+namespace MVC_SYSTEM.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class tbl_Supervisor
+    {
+        [Key]
+        public int fld_ID { get; set; }
+
+        public string fld_SupervisorID { get; set; }
+
+        public string fld_SupervisorName { get; set; }
+
+        public int? fld_NegaraID { get; set; }
+        public int? fld_SyarikatID { get; set; }
+        public int? fld_WilayahID { get; set; }
+        public int? fld_LadangID { get; set; }
+        public int? fld_DivisionID { get; set; }
+        public bool? fld_Deleted { get; set; }
+        public DateTime? fld_CreatedDT { get; set; }
+        public int? fld_CreatedBy { get; set; }
+    }
+}
+
---
+++ MVC_SYSTEM/Models/vw_SupervisorMembersInfo.cs
@@ -0,0 +1,28 @@
+﻿namespace MVC_SYSTEM.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public class vw_SupervisorMembersInfo
+    {
+
+        [Key]
+        public string fld_Nopkj { get; set; }
+        public string fld_Nama { get; set; }
+        public int? fld_NegaraID { get; set; }
+
+        public int? fld_SyarikatID { get; set; }
+
+        public int? fld_WilayahID { get; set; }
+
+        public int? fld_LadangID { get; set; }
+        public int? fld_DivisionID { get; set; }
+        public string fld_Kdaktf { get; set; }
+        public string fld_Ktgpkj { get; set; }
+        public string fldOptConfDesc { get; set; }
+        public string fldOptConfValue { get; set; }
+    }
+}
