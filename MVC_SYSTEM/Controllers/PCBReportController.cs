4020a644f49a9865f1ba354cef9fb62ce94013d6
ashahri90 <ashahri90@gmail.com>
2024-08-08 23:08:40 +0800

    Update PCBReportController.cs

--- MVC_SYSTEM/Controllers/PCBReportController.cs
+++ MVC_SYSTEM/Controllers/PCBReportController.cs
@@ -1899,7 +1899,7 @@ namespace MVC_SYSTEM.Controllers
             cell = new PdfPCell(new Phrase(chunk));
             CellPropoties(cell, table, Element.ALIGN_LEFT, Element.ALIGN_TOP, 0, 2, 10, 5);
 
-            chunk = new Chunk(GetTriager.GetTotalForMoney(pkjPCBInfo.fld_CarumanPekerja), FontFactory.GetFont("Arial", 10, Font.NORMAL, BaseColor.BLACK));
+            chunk = new Chunk(GetTriager.GetTotalForMoney(pkjPCBInfo.fld_CarumanPekerjaNet), FontFactory.GetFont("Arial", 10, Font.NORMAL, BaseColor.BLACK));
             cell = new PdfPCell(new Phrase(chunk));
             cell.BackgroundColor = BaseColor.YELLOW;
             CellPropoties(cell, table, Element.ALIGN_RIGHT, Element.ALIGN_TOP, 0, 1, 10, 5);
@@ -1933,7 +1933,7 @@ namespace MVC_SYSTEM.Controllers
             cell = new PdfPCell(new Phrase(chunk));
             CellPropoties(cell, table, Element.ALIGN_LEFT, Element.ALIGN_TOP, 0, 1, 10, 5);
 
-            chunk = new Chunk(GetTriager.GetTotalForMoney(pkjPCBInfo.fld_CarumanPekerjaNet), FontFactory.GetFont("Arial", 10, Font.BOLD, BaseColor.BLACK));
+            chunk = new Chunk(GetTriager.GetTotalForMoney(pkjPCBInfo.fld_CarumanPekerja), FontFactory.GetFont("Arial", 10, Font.BOLD, BaseColor.BLACK));
             cell = new PdfPCell(new Phrase(chunk));
             cell.BackgroundColor = BaseColor.YELLOW;
             CellPropoties(cell, table, Element.ALIGN_RIGHT, Element.ALIGN_TOP, 0, 1, 10, 1);
@@ -1943,7 +1943,7 @@ namespace MVC_SYSTEM.Controllers
             }
             else
             {
-                text = "Nota : Jumlah PCB/Cukai Setahun RM" +  + ". Pengiraan PCB perlu dilakukan setiap bulan";
+                text = "Nota : Jumlah PCB/Cukai Setahun RM" + "" + ". Pengiraan PCB perlu dilakukan setiap bulan";
             }
             var phrase = new Phrase();
             chunk = new Chunk(text, FontFactory.GetFont("Arial", 10, Font.NORMAL, BaseColor.BLACK));
